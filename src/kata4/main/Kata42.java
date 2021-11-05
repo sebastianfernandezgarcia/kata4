d65613afb48f8221ba9039fe689dda0aa86cd6db
Sebastián Fernández <sebasfernandezgar@gmail.com>
2021-11-05 13:15:29 +0000

    Version 2

--- src/kata4/main/Kata42.java
+++ src/kata4/main/Kata42.java
@@ -10,19 +10,33 @@ import kata4.model.Histogram;
 
 
 public class Kata42 {
-
-
-    public static void main(String[] args) {
         
+    public static List<Mail> input() {
         String nameFile = new String("email.txt");
         List<Mail> mailList = MailHistogramReader.read(nameFile);
+        return mailList;
+    }
+            
+    public static Histogram<String> process(List<Mail> mailList){
+            Histogram<String> histogram = MailHistogramBuilder.built(mailList);
+            return histogram;
+        }
         
-        Histogram<String> histogram = MailHistogramBuilder.built(mailList);
-        
-  
+  public static void output(Histogram<String> histogram) {
         HistogramDisplay histogramDisplay = new HistogramDisplay("HistogramDisplay", histogram);
         histogramDisplay.execute();
+  }
+      
+  
+  public static void execute() {
+      List<Mail> mailList = input();
+      Histogram<String> histogram = process(mailList);
+      output(histogram);
+  }
         
+    public static void main(String[] args) {
+        Kata42 kata4 = new Kata42();
+        kata4.execute();    
     }
     
 }
